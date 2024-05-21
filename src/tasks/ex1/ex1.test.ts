0d4e78c3df6f58042933fa3b4b6b9af89e84632f
Ben Latchford <ben@latchy.net>
2024-05-21 23:37:11 +0100

    Update ex1.test.ts

--- src/tasks/ex1/ex1.test.ts
+++ src/tasks/ex1/ex1.test.ts
@@ -56,13 +56,13 @@ describe('ex1.ts', () => {
             expect(result).toEqual([
                 /**
                  * This animal is declared in the `input.animals.ant` object.
-                 * The name value is equal to the key of the object, which is 'ant'.
-                 * The type value is equal to the `type` value of the object, which is 'insect'.
-                 * The diet value is calculated by using the objects' eats values and the `input.foods` property.
+                 * The 'name' property is equal to the key of the object, which is 'ant'.
+                 * The 'type' property is equal to the `type` value of the object, which is 'insect'.
+                 * The 'diet' property is calculated by using the objects' eats values and the `input.foods` property.
                  *  - If what it eats is found int he `input.foods.herbivorous` array, then the diet is 'herbivorous'.
                  *  - However, if what it eats is found in the `input.foods.carnivorous` array, then the diet is 'carnivorous'.
-                 * The legs value is determined by the `legs` property of the object, if the number is greater than 0, then it has legs.
-                 * The travels value is determined by the `input.moves` property.
+                 * The 'legs' property is determined by the `legs` property of the object, if the number is greater than 0, then it has legs.
+                 * The 'travels' property is determined by the `input.moves` property.
                  *  - The the animals' object `type` value can be used to match the key in the `input.moves` object.
                  *  - Be aware, that the `input.moves` object has values of different types, so you need to check if the 'type' value matches, or is included in the array.
                  */
