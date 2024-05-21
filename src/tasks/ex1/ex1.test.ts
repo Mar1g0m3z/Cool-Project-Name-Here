c9b34ccc7133afcbb5b69fc6e6ce98343eaccefd
Ben Latchford <ben@latchy.net>
2024-05-21 23:35:32 +0100

    Update ex1.test.ts

--- src/tasks/ex1/ex1.test.ts
+++ src/tasks/ex1/ex1.test.ts
@@ -54,7 +54,21 @@ describe('ex1.ts', () => {
             };
             const result = transform(input);
             expect(result).toEqual([
+                /**
+                 * This animal is declared in the `input.animals.ant` object.
+                 * The name value is equal to the key of the object, which is 'ant'.
+                 * The type value is equal to the `type` value of the object, which is 'insect'.
+                 * The diet value is calculated by using the objects' eats values and the `input.foods` property.
+                 *  - If what it eats is found int he `input.foods.herbivorous` array, then the diet is 'herbivorous'.
+                 *  - However, if what it eats is found in the `input.foods.carnivorous` array, then the diet is 'carnivorous'.
+                 * The legs value is determined by the `legs` property of the object, if the number is greater than 0, then it has legs.
+                 * The travels value is determined by the `input.moves` property.
+                 *  - The the animals' object `type` value can be used to match the key in the `input.moves` object.
+                 *  - Be aware, that the `input.moves` object has values of different types, so you need to check if the 'type' value matches, or is included in the array.
+                 */
                 { name: 'ant', type: 'insect', diet: 'herbivorous', legs: true, travels: 'walk' },
+
+                // ...the others
                 { name: 'bird', type: 'bird', diet: 'carnivorous', legs: true, travels: 'fly' },
                 { name: 'cat', type: 'mammal', diet: 'carnivorous', legs: true, travels: 'walk' },
                 { name: 'cow', type: 'mammal', diet: 'herbivorous', legs: true, travels: 'walk' },
