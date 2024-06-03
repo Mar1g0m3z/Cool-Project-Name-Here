e4e7495341b323550068b886e53110af3cfaf9f8
Marisela Gomez <mariselaalejandragomez@gmail.com>
2024-06-02 17:23:39 -0700

    test ex problem 1 and 3

--- src/tasks/ex2/ex2.ts
+++ src/tasks/ex2/ex2.ts
@@ -8,7 +8,7 @@
 export function increase_numbers(numbers: number[], amount: number): number[] {
 	let newArr = [];
 	let increase_nums = numbers.map((num) => {
-		newArr.push(num + 5);
+		newArr.push(num + amount);
 	});
 	return newArr;
 }
@@ -18,9 +18,19 @@ export function increase_numbers(numbers: number[], amount: number): number[] {
  */
 export function transform_people(
 	people: { name: string; age: number }[],
-): { name: string; is_adult: boolean }[] {}
+): { name: string; is_adult: boolean }[] {
+	return [];
+}
 
 /*
  * Return an array containing only names with 4 letters.
  */
-export function filter_names(names: string[]): string[] {}
+export function filter_names(names: string[]): string[] {
+	let short_array = [];
+	let short_names = names.map((name) => {
+		if (name.length === 4 && name) {
+			short_array.push(name);
+		}
+	});
+	return short_array;
+}
